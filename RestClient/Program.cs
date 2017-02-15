74eed71c5263aa258b0a654500f8192ebdfcf25e
kelrezo <travis_layne@yahoo.com>
2017-02-15 13:27:11 -0600

    got the events of the past 6 months

--- RestClient/Program.cs
+++ RestClient/Program.cs
@@ -18,44 +18,59 @@ namespace RestClient
         static void Main(string[] args)
         {
  
-            string url = "https://acadianasoftwaregroup.org/#/";
-            var request = (HttpWebRequest)WebRequest.Create(url);
+            string url1 = "https://acadianasoftwaregroup.org/api/cms?o=10&s=0";
+            string url2 = "https://acadianasoftwaregroup.org/api/user/profiles/{0}";
+            string url3 = "https://acadianasoftwaregroup.org/api/cal/event?s={0}&e={1}";
+            string method = "get";
+            //request = (HttpWebRequest)WebRequest.Create(url);
+            //request.Method = "get";
+            //response = request.GetResponse();
+
+
+            //content = response.GetResponseStream();
+            //responseStream = response.GetResponseStream();
+            //if (responseStream != null)
+            //{
+            //    var reader = new StreamReader(responseStream);
+            //    receiveContent = reader.ReadToEnd();
+            //    reader.Close();
+            //}
+            //dynamic articles = JsonConvert.DeserializeObject(receiveContent);
+            dynamic articles = request(url1, method);
+            List<dynamic> writers = new List<dynamic>();
+            foreach (var article in articles)
+            {
+                Console.WriteLine("Title: " + article.title);
+                Console.WriteLine("Author: " + request(string.Format(url2, article.authorId), method).name);
+                Console.WriteLine("----------------------------------------------------------------------");
+            }
+            Console.WriteLine("\nEvents\n");
+            DateTime end = DateTime.UtcNow;
+            DateTime start = end.AddMonths(-6);
+            dynamic events = request(string.Format(url3, start.ToString("o"), end.ToString("o")), method);
+            foreach (var meet in events)
+            {
+                Console.WriteLine(meet);
+            }
+            Console.ReadLine();
           
-            request.Method = "get";
+        }
+        static dynamic request(string url,string method)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = method;
             var response = request.GetResponse();
             var content = response.GetResponseStream();
             var responseStream = response.GetResponseStream();
-            string receiveContent= "";
-            if (responseStream != null)
-            {
-                var reader = new StreamReader(responseStream);
-                receiveContent = reader.ReadToEnd();
-                reader.Close();              
-            }
-
-            //Console.WriteLine(receiveContent);
-            url = "https://acadianasoftwaregroup.org/api/cms?o=10&s=0";
-            request = (HttpWebRequest)WebRequest.Create(url);
-            request.Method = "get";
-            response = request.GetResponse();
-            
-            content = response.GetResponseStream();
-            responseStream = response.GetResponseStream();
-            receiveContent = "";
+            string receiveContent = "";
             if (responseStream != null)
             {
                 var reader = new StreamReader(responseStream);
                 receiveContent = reader.ReadToEnd();
                 reader.Close();
             }
-            dynamic articles = JsonConvert.DeserializeObject(receiveContent);
-            foreach (var article in articles)
-            {
-                Console.WriteLine(article.title);
-            }
-
-            Console.ReadLine();
-          
+            //dynamic data = JsonConvert.DeserializeObject(receiveContent);
+            return JsonConvert.DeserializeObject(receiveContent);
         }
 
     }
