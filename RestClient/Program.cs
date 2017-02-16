abe2920d031cbb95d56e70db96b83b195719f14b
kelrezo <travis_layne@yahoo.com>
2017-02-16 14:36:39 -0600

    remvoed uncessary classes

--- RestClient/Program.cs
+++ RestClient/Program.cs
@@ -1,14 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 
 namespace RestClient
