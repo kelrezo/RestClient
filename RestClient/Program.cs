9c84ed34cf8aab73740b9f54c142d58035458e38
kelrezo <travis_layne@yahoo.com>
2017-02-16 09:31:02 -0600

    cleaning up comments (done?)

--- RestClient/Program.cs
+++ RestClient/Program.cs
@@ -22,43 +22,28 @@ namespace RestClient
             string url2 = "https://acadianasoftwaregroup.org/api/user/profiles/{0}";
             string url3 = "https://acadianasoftwaregroup.org/api/cal/event?s={0}&e={1}";
             string url4 = "https://acadianasoftwaregroup.org/api/bb/{0}";
-            string url5 = "https://acadianasoftwaregroup.org/api/bb/{0}/thread/{1}";
-            string url6 = "https://acadianasoftwaregroup.org/api/bb/{0}/{1}?o={2}0&s={3}";
-            string url7 = "https://acadianasoftwaregroup.org/api/bb/{0}/{1}";
+            string url5 = "https://acadianasoftwaregroup.org/api/bb/{0}/{1}";
 
-            //request = (HttpWebRequest)WebRequest.Create(url);
-            //request.Method = "get";
-            //response = request.GetResponse();
+            Console.WriteLine("Articles\n");
+            dynamic articles = request(url1);
+            List<dynamic> writers = new List<dynamic>();
+            foreach (var article in articles)
+            {
+                Console.WriteLine("Title: " + article.title);
+                Console.WriteLine("Author: " + request(string.Format(url2, article.authorId)).name);
+                Console.WriteLine("----------------------------------------------------------------------");
+            }
 
+            Console.WriteLine("\nEvents\n");
+            DateTime end = DateTime.UtcNow;
+            DateTime start = end.AddMonths(-6);
+            dynamic events = request(string.Format(url3, start.ToString("o"), end.ToString("o")));
+            foreach (var meet in events)
+            {
+                Console.WriteLine(meet);
+            }
 
-            //content = response.GetResponseStream();
-            //responseStream = response.GetResponseStream();
-            //if (responseStream != null)
-            //{
-            //    var reader = new StreamReader(responseStream);
-            //    receiveContent = reader.ReadToEnd();
-            //    reader.Close();
-            //}
-            //dynamic articles = JsonConvert.DeserializeObject(receiveContent);
-            //dynamic articles = request(url1);
-            //List<dynamic> writers = new List<dynamic>();
-            //foreach (var article in articles)
-            //{
-            //    Console.WriteLine("Title: " + article.title);
-            //    Console.WriteLine("Author: " + request(string.Format(url2, article.authorId)).name);
-            //    Console.WriteLine("----------------------------------------------------------------------");
-            //}
-
-            //Console.WriteLine("\nEvents\n");
-            //DateTime end = DateTime.UtcNow;
-            //DateTime start = end.AddMonths(-6);
-            //dynamic events = request(string.Format(url3, start.ToString("o"), end.ToString("o")));
-            //foreach (var meet in events)
-            //{
-            //    Console.WriteLine(meet);
-            //}
-
-            Console.WriteLine("\nPoster\n");
+            Console.WriteLine("\nPost in first Thread\n");
    
             dynamic baord = request(string.Format(url4,""));
             dynamic boardId;
@@ -75,7 +60,7 @@ namespace RestClient
                 //each thread in board
                 foreach (var thread in threads)
                 {
-                    dynamic posts = request(string.Format(url7, boardId, thread.threadId));
+                    dynamic posts = request(string.Format(url5, boardId, thread.threadId));
                     Console.WriteLine(posts);
                     //each post in thread
                     foreach (var post in posts)
